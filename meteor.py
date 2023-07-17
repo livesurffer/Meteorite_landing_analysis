563dff681f9615a9545213f920602a1dff65ef67
serzh <serjhojen@gmail.com>
2023-07-17 15:16:47 +0530

    retrieve data from NASA dataset

---
+++ meteor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import requests
+import pandas as pd
+
+r = requests.get('https://data.nasa.gov/resource/gh4g-9sfh.json?$limit=50000')
+meteor_data = r.json()
+df = pd.DataFrame(meteor_data)
+print(df.shape)
+#print(meteor_data)
